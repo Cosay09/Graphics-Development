c5a43742fe9a5d752df6f7bf11c09331a44344d8
Cosay09 <childrengang8@gmail.com>
2025-01-16 19:37:15 +0600

    Added Paddle

--- main.cpp
+++ main.cpp
@@ -25,12 +25,16 @@ OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+
 using namespace std;
 
 const int WIDTH = 1280;
 const int HEIGHT = 720;
-const int Ball_Width = 15;
-const int Ball_Height = 15;
+const int Ball_Width = 10;
+const int Ball_Height = 10;
+const int Paddle_Width = 7;
+const int Paddle_Height = 70;
 
 class Vec2
 {
@@ -81,9 +85,33 @@ public:
 	SDL_Rect rect{};
 };
 
+class Paddle
+{
+public:
+    Paddle(Vec2 position) : position(position)
+    {
+        rect.x = static_cast<int>(position.x);
+        rect.y = static_cast<int>(position.y);
+        rect.w = Paddle_Width;
+        rect.h = Paddle_Height;
+    }
+
+    void Draw (SDL_Renderer *renderer)
+    {
+        rect.y = static_cast<int>(position.y);
+
+        SDL_RenderFillRect(renderer, &rect);
+    }
+
+    Vec2 position;
+    SDL_Rect rect;
+};
+
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+
     SDL_Window *window = SDL_CreateWindow(
         "Pong",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -95,13 +123,19 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
+    // Initialize the Text
+    TTF_Font *scoreFont = TTF_OpenFont("Game_Number.ttf", 40);
     // Create Ball
     Ball ball(
         Vec2(WIDTH / 2.0f - Ball_Width / 2.0f, HEIGHT / 2.0f - Ball_Height / 2.0f));
 
+    // Create two Paddle
+    Paddle paddle1(
+        Vec2(WIDTH / 60.0f - Paddle_Width / 2.0f, HEIGHT / 2.0f - Paddle_Height / 2.0f));
+
+    Paddle paddle2(
+        Vec2(WIDTH - (WIDTH / 60.0f), HEIGHT / 2.0f - Paddle_Height / 2.0f));
     // GAME LOGIC
     bool running = true;
     while (running)
@@ -129,8 +163,8 @@ int main(int argc, char *argv[])
         SDL_RenderClear(renderer);
 
         //Set the color to yellow;
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0, 0xFF);
+
         //Draw border
         for (int i = 0; i < HEIGHT; i++)
         {
@@ -139,9 +173,13 @@ int main(int argc, char *argv[])
                 SDL_RenderDrawPoint(renderer, WIDTH / 2, i);
             }
         }
-        
+
         // Draw Ball
         ball.Draw(renderer);
+
+        // Draw Paddles
+        paddle1.Draw(renderer);
+        paddle2.Draw(renderer);
 
         //Present the backbuffer
         SDL_RenderPresent(renderer);
@@ -150,6 +188,9 @@ int main(int argc, char *argv[])
     // CLEANUPS ALWAYS!!!!!!!!!!
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_CloseFont(scoreFont);
+    TTF_Quit();
     SDL_Quit();
+
     return 0;
 }
