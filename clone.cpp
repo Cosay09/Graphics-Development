186d0e933a0280b8589c552eda0b835c3dd90e60
Cosay09 <childrengang8@gmail.com>
2025-02-21 22:03:05 +0600

    Opening a img

--- clone.cpp
+++ clone.cpp
@@ -1,70 +1,108 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 using namespace std;
 
 const int WINDOW_WIDTH = 720;
 const int WINDOW_HEIGHT = 480;
 
+SDL_Window *window = NULL;
+SDL_Surface *surface = NULL;
+SDL_Surface *img1 = NULL;
+
+bool init();
+bool loadmedia();
+void close();
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    SDL_Window *window = SDL_CreateWindow("Replica", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-
-    if (window == NULL)
+    if (!init())
     {
-        cout << "Window was not created." << '\n'
-             << SDL_GetError << '\n';
-        return 1;
+        printf("Could not initialize! SDL Error: %s\n", SDL_GetError());
     }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-    bool running = true;
-    SDL_Event event;
-
-    while (running)
+    else
     {
-        while (SDL_PollEvent(&event))
+        if (!loadmedia())
         {
-            if (event.type == SDL_QUIT)
+            printf("Failed to load media!\n");
+        }
+        else
+        {
+            SDL_BlitSurface(img1, NULL, surface, NULL);
+
+            SDL_UpdateWindowSurface(window);
+
+            SDL_Event event;
+            bool running = true;
+
+            while (running)
             {
-                running = false;
-            }
-            else if (event.type == SDL_KEYDOWN)
-            {
-                if (event.key.keysym.sym == SDLK_ESCAPE)
+                while (SDL_PollEvent(&event))
                 {
-                    running = false;
+                    if (event.type == SDL_QUIT)
+                    {
+                        running = false;
+                    }
+                    if (event.type == SDL_KEYDOWN)
+                    {
+                        if (event.key.keysym.sym == SDLK_ESCAPE)
+                        {
+                            running = false;
+                        }
+                    }
                 }
             }
         }
-
-        // SETS THE SCREEN TO BLACK AND RE_DRAWS EVERYTIME
-        SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xFF);
-        SDL_RenderClear(renderer);
-
-        // Set the color to yellow;
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0, 0xFF);
-
-        // Draw border
-        for (int i = 0; i < WINDOW_HEIGHT; i++)
-        {
-            if (i % 5 != 0)
-            {
-                SDL_RenderDrawPoint(renderer, WINDOW_WIDTH / 2, i);
-            }
-        }
-
-        SDL_LoadBMP( "Screenshot (5).png" );
-        SDL_RenderPresent(renderer);
     }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    close();
 
     return 0;
+}
+
+bool init()
+{
+    bool success = true;
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        printf("SDL could not be initialized! SDL Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+        window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window couldn't be created! SDL Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            surface = SDL_GetWindowSurface(window);
+        }
+    }
+
+    return success;
+}
+
+bool loadmedia()
+{
+    bool success = true;
+
+    img1 = SDL_LoadBMP("OIUGRK1 [Converted].bmp");
+    if (img1 == NULL)
+    {
+        printf("Couldn't load image %s! SDL Error: %s", "OIUGRK1 [Converted].bmp", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    SDL_DestroyWindow(window);
+    SDL_FreeSurface(img1);
+    SDL_Quit();
 }
